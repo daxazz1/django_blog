3a412205477ee0a0ed1811b4a9c901a2f48b5279
daxazz1 <90633516+daxazz1@users.noreply.github.com>
2022-05-11 18:11:35 +0300

    added comments, some fixes

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -16,3 +16,13 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
